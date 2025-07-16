338a39f3822578b04cd695c4de430538dfed35ff
ilix-studio <ilishjyoti17@gmail.com>
2025-07-16 18:40:15 +0530

    Fix CORS error/3

--- src/config/allowOrigins.ts
+++ src/config/allowOrigins.ts
@@ -1,5 +1,8 @@
 const allowOrigins = [
   "http://localhost:5173",
+  "https://www.biswajitphukan.in/",
+  "https://biswajitphukan.in/",
+  "https://www.biswajitphukan.in",
   "https://biswajitphukan.in/admin/login",
   "https://biswajitphukan.in/admin/dashboard",
 ];
